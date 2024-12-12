68e853161333292cbc2734401fcc5b49ef187e94
William Vabrinskas <wvabrinskas@gmail.com>
2024-12-12 11:51:57 -0500

    updated swift syntax

--- Package.swift
+++ Package.swift
@@ -25,11 +25,11 @@ let package = Package(
         ),
     ],
     dependencies: [
-      .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+      .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
     ],
     targets: [
       // Targets are the basic building blocks of a package, defining a module or a test suite.
-      // Targets can depend on other targets in this package and products from dependencies.
+      // Targets can depend on other targets in this packa∫ge and products from dependencies.
       .target(name: "HuddleArch"),
       
       .target(name: "HuddleMacros", dependencies: ["HuddleMacrosMacros"]),
