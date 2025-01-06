b1165d33d0499757fffc699fbbafdcb9bbeac183
William Vabrinskas <wvabrinskas@gmail.com>
2025-01-06 17:06:09 -0500

    added comment

--- Sources/HuddleMacros/HuddleMacros.swift
+++ Sources/HuddleMacros/HuddleMacros.swift
@@ -5,7 +5,9 @@ import HuddleArch
 @attached(member, names: arbitrary, conformances: ComponentProviding)
 public macro ComponentImpl() = #externalMacro(module: "HuddleMacrosMacros", type: "ComponentImplMacro")
 
-/// A macro that expands the Components initializer
+/// A macro that adds the conformance for `ViewBuilding` for a particular builder
+/// router: The Routing type for the specific builder
+/// component: The ViewComponent type for the specific builder.
 @attached(member, names: arbitrary, conformances: ViewBuilding)
 public macro Building(_ router: Routing.Type,
                       _ component: ViewComponent.Type) = #externalMacro(module: "HuddleMacrosMacros", type: "BuildingImplMacro")
